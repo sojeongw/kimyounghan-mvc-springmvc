e3d01cdcda7077695925cff03a09a859c7e3d7e3
sojeongw <sojeongw.official@gmail.com>
2021-07-03 06:11:57 +0900

    로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller라고 하면 View 이름을 반환하고
+// Restcontroller로 하면 "ok"라는 스트링이 그대로 반환된다.
+@RestController
+@Slf4j
+public class LogTestController {
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+}
